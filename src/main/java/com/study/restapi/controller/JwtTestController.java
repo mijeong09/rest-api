fd4ab4fff1d2e48a0da5ffd4fb463124c5b24981
mijeong09 <me9809180@gmail.com>
2025-12-09 10:25:37 +0900

    JWT 발행

---
+++ src/main/java/com/study/restapi/controller/JwtTestController.java
@@ -0,0 +1,30 @@
+package com.study.restapi.controller;
+
+
+import com.study.restapi.dto.response.ApiResponse;
+import com.study.restapi.security.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/jwt-test")
+@RequiredArgsConstructor
+public class JwtTestController {
+
+    private final JwtProvider jwtProvider;
+
+    @GetMapping("/generate")
+    public ResponseEntity<ApiResponse<Map<String, String>>> generateToken(
+            @RequestParam String username
+    ) {
+        String token = jwtProvider.createToken(username);
+        return ResponseEntity.ok(ApiResponse.success(Map.of("toke", token)));
+    }
+
+}
